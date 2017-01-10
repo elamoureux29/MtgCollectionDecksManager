868deddbeae0ff944b749142f31c8e69ca836003
Erik Lamoureux <Erik Lamoureux>
2017-01-10 12:12:18 -0500

    Initial commit

---
+++ src/com/app/java/Main.java
@@ -0,0 +1,10 @@
+package com.app.java;
+
+/**
+ * Created by erik on 1/8/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
